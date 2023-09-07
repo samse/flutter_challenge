e6c8ae9d2e5f3b74a29a0f30ea8b188e8ee5e9e6
Yosam Lee <yslee.gm@gmail.com>
2023-09-07 17:23:58 +0900

    하단 작업중...

--- twit_clone/twitter_clone/lib/features/home/subviews/attach_file_screen.dart
+++ twit_clone/twitter_clone/lib/features/home/subviews/attach_file_screen.dart
@@ -9,6 +9,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:twitter_clone/app.dart';
 import 'package:twitter_clone/common/gaps.dart';
 import 'package:twitter_clone/common/sizes.dart';
 
@@ -121,6 +122,19 @@ class _AttachFileScreenState extends State<AttachFileScreen>
     }
   }
 
+  bool _isCameraMode = true;
+  void _onTapCamera(BuildContext context) {
+    setState(() {
+      _isCameraMode = true;
+    });
+  }
+
+  void _onTapLibrary(BuildContext context) {
+    setState(() {
+      _isCameraMode = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -130,205 +144,216 @@ class _AttachFileScreenState extends State<AttachFileScreen>
         width: size.width,
         height: size.height,
         child: !_hasPermission
-            ? const Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Initializing...",
-                    style:
-                        TextStyle(color: Colors.white, fontSize: Sizes.size20),
-                  ),
-                  Gaps.v20,
-                  CircularProgressIndicator.adaptive()
-                ],
-              )
+            ? EmptyScreen()
             : Stack(
                 alignment: Alignment.center,
                 children: [
                   if (!_noCamera && _cameraController.value.isInitialized)
                     CameraPreview(_cameraController),
-                  Positioned(
-                    top: Sizes.size40,
-                    left: Sizes.size20,
-                    child: SizedBox(
-                      width: size.width,
-                      height: 50,
-                      child: GestureDetector(
-                        onTap: () => context.pop(),
-                        child: const Align(
-                          alignment: Alignment.centerLeft,
-                          child: Icon(
-                            Icons.arrow_back_ios,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
+                  // CameraBackground(),
+                  CloseButton(),
+                  ControlButtons(
+                    takePicture: _takePicture,
+                    toggleSelfieMode: _toggleSelfieMode,
                   ),
-                  Positioned(
-                    bottom: 0,
-                    child: Padding(
-                      padding: const EdgeInsets.only(bottom: 20.0),
-                      child: SizedBox(
-                        width: size.width,
-                        height: 80,
-                        child: Container(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              GestureDetector(
-                                onTap: () {},
-                                child: const FaIcon(
-                                  FontAwesomeIcons.boltLightning,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              GestureDetector(
-                                onTap: () => _takePicture(context),
-                                child: SizedBox(
-                                  width: 80,
-                                  height: 80,
-                                  child: Stack(
-                                    children: [
-                                      Container(
-                                        width: 80,
-                                        height: 80,
-                                        decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(40),
-                                            color: Colors.white),
-                                      ),
-                                      Positioned(
-                                        top: 5,
-                                        left: 5,
-                                        child: Container(
-                                          width: 70,
-                                          height: 70,
-                                          decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(40),
-                                            border: Border.all(
-                                                color: Colors.black, width: 6),
-                                            color: Colors.white,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              GestureDetector(
-                                onTap: () => _toggleSelfieMode,
-                                child: const FaIcon(
-                                  FontAwesomeIcons.rotate,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  )
+                  BottomButtons(
+                      onTapCamera: (context) => _onTapCamera(context),
+                      onTapLibrary: (context) => _onTapLibrary(context),
+                      isCameraMode: _isCameraMode),
                 ],
               ),
       ),
     );
+  }
+}
 
-    //   Container(
-    //   width: size.width,
-    //   height: size.height,
-    //   color: Colors.black,
-    //   child: SafeArea(
-    //     child: Stack(
-    //       alignment: Alignment.center,
-    //       children: [
-    //         if (!_noCamera && _cameraController.value.isInitialized)
-    //           CameraPreview(_cameraController),
-    //         Positioned(
-    //           top: 0,
-    //           left: 10,
-    //           child: SizedBox(
-    //             width: size.width,
-    //             height: 50,
-    //             child: GestureDetector(
-    //               onTap: () => context.pop(),
-    //               child: const Align(
-    //                 alignment: Alignment.centerLeft,
-    //                 child: Icon(
-    //                   Icons.arrow_back_ios,
-    //                   color: Colors.white,
-    //                 ),
-    //               ),
-    //             ),
-    //           ),
-    //         ),
-    //         Positioned(
-    //           bottom: 0,
-    //           child: Padding(
-    //             padding: const EdgeInsets.only(bottom: 20.0),
-    //             child: SizedBox(
-    //               width: size.width,
-    //               height: 80,
-    //               child: Container(
-    //                 child: Row(
-    //                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    //                   children: [
-    //                     GestureDetector(
-    //                       onTap: () {},
-    //                       child: FaIcon(
-    //                         FontAwesomeIcons.boltLightning,
-    //                         color: Colors.white,
-    //                       ),
-    //                     ),
-    //                     SizedBox(
-    //                       width: 80,
-    //                       height: 80,
-    //                       child: Stack(
-    //                         children: [
-    //                           Container(
-    //                             width: 80,
-    //                             height: 80,
-    //                             decoration: BoxDecoration(
-    //                                 borderRadius: BorderRadius.circular(40),
-    //                                 color: Colors.white),
-    //                           ),
-    //                           Positioned(
-    //                             top: 5,
-    //                             left: 5,
-    //                             child: Container(
-    //                               width: 70,
-    //                               height: 70,
-    //                               decoration: BoxDecoration(
-    //                                 borderRadius: BorderRadius.circular(40),
-    //                                 border: Border.all(
-    //                                     color: Colors.black, width: 6),
-    //                                 color: Colors.white,
-    //                               ),
-    //                             ),
-    //                           ),
-    //                         ],
-    //                       ),
-    //                     ),
-    //                     GestureDetector(
-    //                       onTap: () {
-    //                         print("_toggleSelfieMode");
-    //                       },
-    //                       child: FaIcon(
-    //                         FontAwesomeIcons.rotate,
-    //                         color: Colors.white,
-    //                       ),
-    //                     ),
-    //                   ],
-    //                 ),
-    //               ),
-    //             ),
-    //           ),
-    //         )
-    //       ],
-    //     ),
-    //   ),
-    // );
+const kBottomControlHeight = 200;
+
+// class CameraBackground extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     Size size = MediaQuery.of(context).size;
+//     return Container(
+//       width: size.width,
+//       height: size.height,
+//       color: Colors.transparent,
+//       child: Container(
+//         width: size.width,
+//         height: size.height - kBottomControlHeight,
+//         color: Colors.transparent,
+//       ),
+//     );
+//   }
+// }
+
+class EmptyScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Initializing...",
+          style: TextStyle(color: Colors.white, fontSize: Sizes.size20),
+        ),
+        Gaps.v20,
+        CircularProgressIndicator.adaptive()
+      ],
+    );
+  }
+}
+
+class CloseButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Positioned(
+      top: Sizes.size40,
+      left: Sizes.size20,
+      child: SizedBox(
+        width: size.width,
+        height: 50,
+        child: GestureDetector(
+          onTap: () => context.pop(),
+          child: const Align(
+            alignment: Alignment.centerLeft,
+            child: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BottomButtons extends StatelessWidget {
+  final void Function(BuildContext) onTapCamera;
+  final void Function(BuildContext) onTapLibrary;
+  final bool isCameraMode;
+
+  const BottomButtons(
+      {super.key,
+      required this.onTapCamera,
+      required this.onTapLibrary,
+      required this.isCameraMode});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Positioned(
+        bottom: 0,
+        child: Container(
+          width: size.width,
+          height: 130,
+          color: Colors.black.withOpacity(0.5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Colors.transparent,
+                ),
+              ),
+              GestureDetector(
+                onTap: () => onTapCamera(context),
+                child: Text(
+                  "Camera",
+                  style: context.buttonTitle!.copyWith(
+                      color: isCameraMode ? Colors.white : Colors.grey),
+                ),
+              ),
+              Gaps.h60,
+              GestureDetector(
+                onTap: () => onTapLibrary(context),
+                child: Text(
+                  "Library",
+                  style: context.buttonTitle!.copyWith(
+                      color: !isCameraMode ? Colors.white : Colors.grey),
+                ),
+              ),
+              Gaps.h52,
+            ],
+          ),
+        ));
+  }
+}
+
+class ControlButtons extends StatelessWidget {
+  final Function takePicture;
+  final Function toggleSelfieMode;
+
+  const ControlButtons(
+      {super.key, required this.takePicture, required this.toggleSelfieMode});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Positioned(
+      bottom: 130,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 20.0),
+        child: SizedBox(
+          width: size.width,
+          height: 80,
+          child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: () {},
+                  child: const FaIcon(
+                    FontAwesomeIcons.boltLightning,
+                    color: Colors.white,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => takePicture(context),
+                  child: SizedBox(
+                    width: 80,
+                    height: 80,
+                    child: Stack(
+                      children: [
+                        Container(
+                          width: 80,
+                          height: 80,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(40),
+                              color: Colors.white),
+                        ),
+                        Positioned(
+                          top: 5,
+                          left: 5,
+                          child: Container(
+                            width: 70,
+                            height: 70,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(40),
+                              border: Border.all(color: Colors.black, width: 6),
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => toggleSelfieMode,
+                  child: const FaIcon(
+                    FontAwesomeIcons.rotate,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
