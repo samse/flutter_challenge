19352c01a347fbf3e3e43ace9d9f82fd626b2984
Yosam Lee <yslee.gm@gmail.com>
2023-09-08 17:36:42 +0900

    tween anim 적용하여 redarc만 애니매이션 적용.

--- animation_class/watch/watch/lib/main.dart
+++ animation_class/watch/watch/lib/main.dart
@@ -14,19 +14,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Apple watch',
-      home: const AppleWatch(title: 'Flutter Demo Home Page'),
+      home: AppleWatch(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class AppleWatch extends StatefulWidget {
-  const AppleWatch({super.key, required this.title});
+  AppleWatch({super.key, required this.title});
   final String title;
+  final redValue = Random().nextDouble() * 2.0;
+  final greenalue = Random().nextDouble() * 2.0;
+  final blueValue = Random().nextDouble() * 2.0;
+
   @override
   State<AppleWatch> createState() => _AppleWatchState();
 }
 
-class _AppleWatchState extends State<AppleWatch> {
+class _AppleWatchState extends State<AppleWatch>
+    with SingleTickerProviderStateMixin {
+  double redValue = Random().nextDouble() * 2.0;
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: Duration(milliseconds: 300),
+    lowerBound: 0.005,
+    upperBound: 2.0,
+  );
+
+  void _onTap() {
+    setState(() {
+      redValue = Random().nextDouble() * 2.0;
+      print("_OnTap value = $redValue");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,9 +56,33 @@ class _AppleWatchState extends State<AppleWatch> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: CustomPaint(
-          painter: AppleWatchPainter(),
-          size: const Size(300, 300),
+        child: Column(
+          children: [
+            TweenAnimationBuilder(
+              tween: Tween<double>(begin: 0, end: redValue),
+              duration: Duration(milliseconds: 300),
+              builder: (context, double value, child) {
+                return CustomPaint(
+                  painter: AppleWatchPainter(
+                      redValue: value,
+                      greenValue: widget.greenalue,
+                      blueValue: widget.blueValue),
+                  size: const Size(300, 300),
+                );
+              },
+            ),
+            GestureDetector(
+              onTap: _onTap,
+              child: Container(
+                padding: const EdgeInsets.all(10.0),
+                color: Colors.cyan,
+                child: Text(
+                  "Next",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
@@ -46,41 +90,75 @@ class _AppleWatchState extends State<AppleWatch> {
 }
 
 class AppleWatchPainter extends CustomPainter {
-  final barWidth = 32.0;
-  final barPeriod = 2.0;
+  final barWidth = 28.0;
+  final barPeriod = 4.0;
+
+  late double? redValue;
+  late double? greenValue;
+  late double? blueValue;
+
+  AppleWatchPainter({this.redValue, this.greenValue, this.blueValue});
+
   @override
   void paint(Canvas canvas, Size size) {
-    final paint = Paint()..color = Colors.white;
-    //canvas.drawRect(Rect.fromLTWH(0, 0, size.width, size.height), paint);
+    final centerOffset = Offset(size.width / 2, size.height / 2);
 
-    final blackPaint = Paint()..color = Colors.black;
-    final redPaint = Paint()..color = Colors.red.shade900.withOpacity(0.5);
-    final greenPaint = Paint()..color = Colors.green.shade900;
-    final bluePaint = Paint()..color = Colors.cyan.shade900;
-    final offset = Offset(size.width / 2, size.height / 2);
-    final halfWidth = size.width / 2;
-    canvas.drawCircle(offset, halfWidth, redPaint);
-    canvas.drawCircle(offset, halfWidth - barWidth, blackPaint);
-    canvas.drawCircle(offset, halfWidth - barWidth - barPeriod, greenPaint);
-    canvas.drawCircle(
-        offset, halfWidth - (barWidth * 2) - barPeriod, blackPaint);
-    canvas.drawCircle(
-        offset, halfWidth - (barWidth * 2) - (barPeriod * 2), bluePaint);
-    canvas.drawCircle(
-        offset, halfWidth - (barWidth * 3) - (barPeriod * 2), blackPaint);
+    final nPaint = Paint()
+      ..color = Colors.red.shade900.withOpacity(0.5)
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = barWidth;
 
-    final redArcRect = Rect.fromCenter(
-        center: offset, width: size.width - 30, height: size.height - 30);
-    final redArcPaint = Paint()
+    final arcPaint = Paint()
       ..color = Colors.red.shade400
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round
       ..strokeWidth = barWidth;
-    canvas.drawArc(redArcRect, -0.5 * pi, 1.5 * pi, false, redArcPaint);
+
+    final redSize = size.width - 30;
+    drawArcBar(
+        canvas: canvas,
+        arcRect: Rect.fromCenter(
+            center: centerOffset, width: redSize, height: redSize),
+        paint: nPaint,
+        arcPaint: arcPaint,
+        value: redValue ?? -0.4);
+
+    nPaint.color = Colors.green.shade900.withOpacity(0.5);
+    arcPaint.color = Colors.green.shade400;
+    final greenSize = redSize - barWidth * 2 - 10;
+    drawArcBar(
+        canvas: canvas,
+        arcRect: Rect.fromCenter(
+            center: centerOffset, width: greenSize, height: greenSize),
+        paint: nPaint,
+        arcPaint: arcPaint,
+        value: greenValue ?? -0.4);
+    nPaint.color = Colors.cyan.shade900.withOpacity(0.5);
+    arcPaint.color = Colors.cyan.shade400;
+    final blueSize = redSize - barWidth * 4 - 20;
+    drawArcBar(
+        canvas: canvas,
+        arcRect: Rect.fromCenter(
+            center: centerOffset, width: blueSize, height: blueSize),
+        paint: nPaint,
+        arcPaint: arcPaint,
+        value: blueValue ?? -0.4);
+  }
+
+  void drawArcBar(
+      {required Canvas canvas,
+      required Rect arcRect,
+      required Paint paint,
+      required Paint arcPaint,
+      required double value}) {
+    print("drawArcBar : $value");
+    canvas.drawArc(arcRect, -0.5 * pi, 2 * pi, false, paint);
+    canvas.drawArc(arcRect, -0.5 * pi, value * pi, false, arcPaint);
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
+    return true;
   }
 }
